24263ccb39c891c5ad840625e9f887b552d02cd4
Asadulloh <asadullosadirdinov@gmail.com>
2024-01-23 19:33:33 +0500

    feat: implement linters

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,13 +1,14 @@
+import "dotenv/config";
 import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 
 const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "asadullohpro",
-  password: "asadullohpro",
-  database: "tryspeaking",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export const db: NodePgDatabase = drizzle(pool);
